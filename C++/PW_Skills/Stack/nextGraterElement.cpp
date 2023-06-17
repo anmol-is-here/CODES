5ffe814ebc936539bcf5c19a0d02d54797d815ce
anmol-is-here <anmol.21b1531165@abes.ac.in>
2023-06-17 20:45:08 +0530

    pge-still-not -working

--- C++/PW_Skills/Stack/nextGraterElement.cpp
+++ C++/PW_Skills/Stack/nextGraterElement.cpp
@@ -22,6 +22,32 @@ vector <int> nge(vector<int> &arr){ // functioin to calculate next gratest eleme
     return output;
 }
 
+vector <int> pge(vector<int> &arr){ // functioin to calculate next gratest element.
+    int n = arr.size();
+    vector<int> output(n, -1);
+    stack<int> st;
+    st.push(0); // st-> [1, 2, 3, 5, 8, 9]
+    for(int i=1; i<n; i++){
+        while(!st.empty() and arr[i] > arr[st.top()]) {
+            // output[st.top()] = arr[i];  // output-> [6, -1, -1, -1, 9, -1, 6, 7, -1, -1]
+            int z = st.top();
+            st.pop();
+            if(!st.empty()){
+                output[z] = arr[st.top()];
+            }
+            else{
+                output[z] = -1;
+            }
+        }
+        st.push(i);
+    }
+    while(!st.empty()){  // not mendetory while loop.
+        output[st.top()] = -1;
+        st.pop();
+    }
+    return output;
+}
+
 vector <int> nse(vector<int> &arr){ // function to calculate next smaller element.
     int n = arr.size();
     vector<int> output(n, -1);
@@ -41,9 +67,37 @@ vector <int> nse(vector<int> &arr){ // function to calculate next smaller elemen
     return output;
 }
 
+vector <int> pse(vector<int> &arr){ // function to calculate previous smaller element.
+    int n = arr.size();
+    vector<int> output(n, -1);
+    stack<int> st;
+    st.push(0);
+    for(int i=1; i<n; i++){
+        while(!st.empty() and arr[i] < arr[st.top()]) {
+            output[st.top()] = arr[i];
+            int z = st.top();
+            st.pop();
+            if(!st.empty()){
+                output[z] = arr[st.top()];
+            }
+            else{
+                output[z] = -1;
+            }
+        }
+        st.push(i);
+    }
+    while(!st.empty()){  // not mendetory while loop.
+        output[st.top()] = -1;
+        st.pop();
+    }   
+    return output;
+}
+
+
+
 int main() {   //  0 1 2 3 4 5 6 7 8 9
     vector<int> v {4,6,3,1,0,9,5,6,7,3};
-    vector<int> res = nge(v);
+    vector<int> res = pge(v);
     for(int i = 0; i<res.size(); i++){
         cout<<res[i]<<" ";
     }
