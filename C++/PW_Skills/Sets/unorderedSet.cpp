fefe43696e0ddbe8b65c9aca7e73a4a2e840edc6
anmol-is-here <anmol.21b1531165@abes.ac.in>
2023-06-23 23:07:42 +0530

    SetsType-(1)

---
+++ C++/PW_Skills/Sets/unorderedSet.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+    unordered_set<int> s1;
+
+    s1.insert(3);
+    s1.insert(1);
+    s1.insert(2);
+    s1.insert(1);
+
+    for (auto val:s1){
+        cout<<val<<" ";
+    }cout<<endl;
+    
+    return 0;
+}
