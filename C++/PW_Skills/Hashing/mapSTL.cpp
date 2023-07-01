045618c04c9af14e742d64d5f7dccabdf2407066
anmol-is-here <anmol.21b1531165@abes.ac.in>
2023-07-01 20:13:17 +0530

    STL-(1)

---
+++ C++/PW_Skills/Hashing/mapSTL.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    map<string, int> directory;
+    directory["anshil"] = 489413;
+    directory["aditya"] = 564654;
+    directory["mannu"] = 654516;
+    directory["arjun"] = 155145;
+
+    for(auto ele:directory){
+        cout<<"Name - "<<ele.first<<endl;
+        cout<<"Phone no. - "<<ele.second<<endl;
+    }cout<<endl;
+    // another way to insert values in a directory is.
+    directory["mannu"] = 565115;   // it will not create another key with same name, it only update value of older key.
+    // directory.insert(make_pair("mannu", 256348));  // if value is present witn same key it will not do anything
+    for(auto ele:directory){
+        cout<<"Name - "<<ele.first<<endl;
+        cout<<"Phone no. - "<<ele.second<<endl;
+    }cout<<endl;  
+    
+    return 0;
+}
